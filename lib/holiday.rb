ca8a41cd72aa5d0acacb76e144420a6dedf6e5b4
Veronica Bustabad <vbustabad@gmail.com>
2018-03-04 02:08:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,4 +69,6 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.detect do |holiday_name, supplies|
+  end
 end
